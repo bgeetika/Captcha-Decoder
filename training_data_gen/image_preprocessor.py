5c3c0f31d8065006fd12291e40ebf1716e35bf8f
sujeet <geetikagarg07@gmail.com>
2015-10-04 08:16:20 +0000

    .

--- training_data_gen/image_preprocessor.py
+++ training_data_gen/image_preprocessor.py
@@ -1,12 +1,12 @@
 import numpy
 
 class ImagePreprocessor(object):
-  @staticmethod
-  def GetProcessedImageShape(image_shape):
+  @classmethod
+  def GetProcessedImageShape(cls, image_shape):
     assert len(image_shape) == 3 and image_shape[2] >= 3, image_shape
     return (3, image_shape[0], image_shape[1])
 
-  @staticmethod
-  def ProcessImage(numpy_arr):
+  @classmethod
+  def ProcessImage(cls, numpy_arr):
     assert len(numpy_arr.shape) == 3, numpy_arr.shape
     return numpy.rollaxis(numpy_arr, 2, 0)[:3, :, :]
--- training_data_gen/training_data_generator.py
+++ training_data_gen/training_data_generator.py
@@ -5,21 +5,21 @@ import sys
 from PIL import Image
 
 from training_data_gen.image_preprocessor import ImagePreprocessor
-import training_data_gen.vocabulary
+import training_data_gen.vocabulary as vocabulary
 
 CAPTCHA_FILENAME_PATTERN = re.compile('^\d+_(.*)\..+$')
 def _ParseCaptchaFromImageFilename(image_filepath):
-    image_filename = os.path.basename(image_filepath)
-    match = CAPTCHA_FILENAME_PATTERN.match(image_filename)
-    assert match is not None, image_filename
-    return match.group(1)
+  image_filename = os.path.basename(image_filepath)
+  match = CAPTCHA_FILENAME_PATTERN.match(image_filename)
+  assert match is not None, image_filename
+  return match.group(1)
 
 def _GetCaptchaIdsFromImageFilename(image_filepath):
-    captcha_str = _ParseCaptchaFromImageFilename(image_filepath)
-    captcha_ids = numpy.zeros(len(captcha_str))
-    for i, captcha_char in enumerate(captcha_str):
-        captcha_ids[i] = vocabulary.CHAR_VOCABULARY[captcha_char]
-    return captcha_ids
+  captcha_str = _ParseCaptchaFromImageFilename(image_filepath)
+  captcha_ids = numpy.zeros(len(captcha_str), dtype=numpy.int)
+  for i, captcha_char in enumerate(captcha_str):
+      captcha_ids[i] = vocabulary.CHAR_VOCABULARY[captcha_char]
+  return captcha_ids
 
 
 def _GetImageFilePathsUnderDir(captchas_dir):
@@ -35,38 +35,52 @@ def _GetShapeOfImagesUnderDir(captchas_dir):
   return None
 
 class TrainingData(object):
-  @staticmethod
-  def SaveTrainingData(file_path, image_data, chars):
+  @classmethod
+  def SaveTrainingData(cls, file_path, image_data, chars):
     numpy.savez(file_path, image_data=image_data, chars=chars)
 
-  @staticmethod
-  def LoadTrainingData(file_path):
+  @classmethod
+  def LoadTrainingData(cls, file_path):
     training_data = numpy.load(file_path)
     return training_data['image_data'], training_data['chars']
 
-  @staticmethod
-  def GenerateTrainingData(captchas_dir,
+  @classmethod
+  def GenerateTrainingData(cls,
+                           captchas_dir,
 			   training_data_dir,
-			   max_size=50000,
+			   max_size=35000,
 			   max_captcha_length=5):
     image_shape = _GetShapeOfImagesUnderDir(captchas_dir)
     training_data_shape = tuple(
 	[max_size] + list(ImagePreprocessor.GetProcessedImageShape(image_shape)))
-    training_image_data = numpy.zeros(training_data_shape)
-    training_labels = numpy.zeros((max_size, max_captcha_length))
+    training_image_data = numpy.zeros(training_data_shape, dtype=numpy.float32)
+    training_labels = numpy.zeros((max_size, max_captcha_length), dtype=numpy.int)
 
-    for i, captcha_filepath in enumerate(_GetImageFilePathsUnderDir(captchas_dir)):
+    i = 0
+    for captcha_filepath in _GetImageFilePathsUnderDir(captchas_dir):
+      try:
+        image_data = numpy.asarray(Image.open(captcha_filepath))
+      except Exception as e:
+        print e, captcha_filepath
+        continue
+
+      i += 1
       index = i % max_size
-      image_data = numpy.asarray(Image.open(captcha_filepath))
       training_image_data[index] = ImagePreprocessor.ProcessImage(image_data)
       captcha_ids = _GetCaptchaIdsFromImageFilename(captcha_filepath)
-      training_labels[index, :] = numpy.zeros(max_captcha_length)
+      training_labels[index, :] = numpy.zeros(max_captcha_length, dtype=numpy.int)
       training_labels[index, :captcha_ids.shape[0]] = captcha_ids
-      
+   
+      if i != 0 and (i % 10000) == 0:
+         print 'Generated {0} examples.'.format(i)
+
       if i != 0 and i % max_size == 0:
 	file_path = os.path.join(
 	    training_data_dir, "training_images_{0}.npy".format(i / max_size))
-        SaveTrainingData(file_path, training_image_data)
+        try:
+          cls.SaveTrainingData(file_path, training_image_data, training_labels)
+        except Exception as e:
+          print e
 
 
 def main():
